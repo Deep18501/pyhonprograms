738c3f78942d806fc7a696331c12e48ad06e93d2
Replit user <>
2023-07-16 02:56:56 +0530

    add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# This entrypoint file to be used in development. Start by reading README.md
 import prob_calculator
-from unittest import main
 
 prob_calculator.random.seed(95)
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
@@ -12,5 +10,3 @@ probability = prob_calculator.experiment(
     num_experiments=3000)
 print("Probability:", probability)
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,43 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(self,**balls):
+    self.contents=[]
+    for key,value in balls.items():
+        self.contents.extend([key] * value)
 
-
+  def draw(self,num):
+    drawn=[]
+    if num>= len(self.contents):
+      temp=self.contents.copy()
+      self.contents.clear()
+      return temp
+    for i in range(num):
+      drawn.append(self.contents.pop(random.randint(0, len(self.contents)-1)))
+    return drawn
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  succ=0
+  expected=[]
+  # exlen=num_balls_drawn-len(expected)
+  for k,v in expected_balls.items():
+    expected.extend([k] * v)
+  
+  # print(expected)
+  for i in range(num_experiments):
+      hat1=copy.deepcopy(hat)
+      drawn=hat1.draw(num_balls_drawn)
+      # print(expected)
+      # print(drawn)
+      # for b in expected:
+      #   try :
+      #     drawn.remove(b)
+      #   except:
+      #     break
+      # if len(drawn)==exlen:
+      #   succ+=1
+      if(all(x in drawn for x in expected)):
+        succ+=1
+      
+      
+  # print(succ)
+  return round(succ/num_experiments,3)
